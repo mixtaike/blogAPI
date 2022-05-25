8ce6be0c8bbb606c99daef23ea47be08fbeb0905
Mixtaike <mixtaike@gmail.com>
2022-05-25 15:19:09 -0300

    Autenticação de usuários gravados no banco de dados

--- src/main/java/com/springboot/blog/config/SecurityConfig.java
+++ src/main/java/com/springboot/blog/config/SecurityConfig.java
@@ -1,8 +1,12 @@
 package com.springboot.blog.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.session.RedisSessionProperties.ConfigureAction;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,11 +18,20 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import com.springboot.blog.security.CustomUserDetailsService;
+
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 	
+	@Autowired
+	private CustomUserDetailsService userDetailsService;
+	
+	
+	
+	
+
 	@Bean
 	PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
@@ -39,14 +52,12 @@ public class SecurityConfig {
 
 	  }
 		
+	 //autenticando usuario 
+	private void configure(AuthenticationManagerBuilder auth)throws Exception {
+		
+		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+	}
 	  
-	  @Bean
-	  protected UserDetailsService userdetailsService() {
-		UserDetails julia =   User.builder().username("juliaa").password(passwordEncoder().encode("password")).roles("USER").build();
-		UserDetails admin =   User.builder().username("admin").password(passwordEncoder().encode("adminn")).roles("ADMIN").build();
-		return new InMemoryUserDetailsManager(julia, admin);
-	  }
-
 	
 }
 
