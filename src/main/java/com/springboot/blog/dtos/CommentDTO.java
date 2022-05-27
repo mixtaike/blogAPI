afbd606e61ef8a84cb28bcce4d26719bb040cff5
Mixtaike <mixtaike@gmail.com>
2022-05-27 12:18:09 -0300

    Customizando Swagger Models - UI com anotações PARTE 2

--- src/main/java/com/springboot/blog/dtos/CommentDTO.java
+++ src/main/java/com/springboot/blog/dtos/CommentDTO.java
@@ -5,23 +5,31 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 
+@Api(value = "Comment model information")
 @Data
 public class CommentDTO {
 	
+	
+	@ApiModelProperty(value = "Comment id")
 	private Long id;
 	
+	@ApiModelProperty(value = "Comment name")
 	//nome should not be null or empty
 	@NotEmpty(message = "Name should not be null or empty")
 	private String name;
 	
+	@ApiModelProperty(value = "Comment email")
 	//email field validation
 	@NotEmpty(message = "Email should not be null or empty")
 	@Email(message = "Email format is not correct")
 	private String email;
 	
+	@ApiModelProperty(value = "Comment body")
 	@NotEmpty
 	@Size(min = 10, message = "Coment body must be minimum at least 10 characters")
 	private String body;
--- src/main/java/com/springboot/blog/dtos/PostDTO.java
+++ src/main/java/com/springboot/blog/dtos/PostDTO.java
@@ -5,29 +5,39 @@ import java.util.Set;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+@ApiModel(description = "Post model information")
 @Data
 public class PostDTO {
+	
+	
+	
+	@ApiModelProperty(value = "Blog post id")
 	private Long id;
 	
-	
+	@ApiModelProperty(value = "Blog post title")
 	// title should not be null or empty
 	// title should have at least 2 characters
 	@NotEmpty
 	@Size(min = 2, message = "Post title should have at least 2 characters")
 	private String title;
 	
+	@ApiModelProperty(value = "Blog post description")
 	//post description should not be null or empty
 	//post description should have at least 10 characters
 	@NotEmpty
 	@Size(min = 10, message = "Post description should have at least 10 characters")
 	private String description;
 	
+	@ApiModelProperty(value = "Blog post content")
 	//post content should not be null or empty
 	@NotEmpty
 	private String content;
 	
+	@ApiModelProperty(value = "Blog post comments")
 	private Set<CommentDTO> comments;
 	
 	
